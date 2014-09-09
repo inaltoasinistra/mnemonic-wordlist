eb8d727148a6b309a5939e8b16aadc0f2c4efca4
Martino <martino.salvetti@trilogis.it>
2014-09-09 14:07:21 +0200

    Add total # of words

--- catrates.sh
+++ catrates.sh
@@ -1,7 +1,10 @@
 
-echo -n "No "
-grep -e "^0 " etimo.txt-\(rs-4\).4.enum | wc -l
-echo -n "Ni "
-grep -e "^1 " etimo.txt-\(rs-4\).4.enum | wc -l
-echo -n "Si "
-grep -e "^2 " etimo.txt-\(rs-4\).4.enum | wc -l
+C0=$(grep -e "^0 " etimo.txt-\(rs-4\).4.enum | wc -l)
+C1=$(grep -e "^1 " etimo.txt-\(rs-4\).4.enum | wc -l)
+C2=$(grep -e "^2 " etimo.txt-\(rs-4\).4.enum | wc -l)
+
+echo No $C0
+echo Ni $C1
+echo Si $C2
+
+echo $C0 + $C1 + $C2 | bc
