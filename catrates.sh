0c161ee132afbf051fd90a1dbaf364321613e57a
Martino <martino.salvetti@trilogis.it>
2014-09-09 11:46:07 +0200

    Add script

---
+++ catrates.sh
@@ -0,0 +1,7 @@
+
+echo -n "No "
+grep -e "^0 " etimo.txt-\(rs-4\).4.enum | wc -l
+echo -n "Ni "
+grep -e "^1 " etimo.txt-\(rs-4\).4.enum | wc -l
+echo -n "Si "
+grep -e "^2 " etimo.txt-\(rs-4\).4.enum | wc -l
