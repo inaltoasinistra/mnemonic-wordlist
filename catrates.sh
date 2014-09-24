e4e7783fe5acb6718c577999f24e32dbffc14113
Martino <martino.salvetti@trilogis.it>
2014-09-24 17:06:20 +0200

    Rated some words

--- catrates.sh
+++ catrates.sh
@@ -1,6 +1,6 @@
 
-FNAME=etimo.txt-rs-4.4.enum
-C0=$(grep -e "^0 " $FNAME | wc -l)
+FNAME=etimo.txt-rs-3.4.grouped
+C0=$(grep -e "^[03] " $FNAME | wc -l)
 C1=$(grep -e "^1 " $FNAME | wc -l)
 C2=$(grep -e "^2 " $FNAME | wc -l)
 
